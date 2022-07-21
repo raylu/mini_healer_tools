1af0a1007e32944337f9cf9c077027ff1363752b
raylu <90059+raylu@users.noreply.github.com>
2022-07-20 23:03:37 -0700

    use talent_fixups to make hardlinks to icons

--- extract.py
+++ extract.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import json
 import os
 import subprocess
 import sys
+import urllib.request
 
 def main():
 	asset_ripper_path = sys.argv[1]
@@ -13,6 +15,43 @@ def main():
 		pass
 
 	subprocess.run([asset_ripper_path, 'raw/', '-o' 'extracted'], check=True)
+
+	url = 'https://gitlab.com/ezrast/mini-builder/-/raw/main/scripts/talent_fixups.json'
+	with urllib.request.urlopen(url) as r:
+		assert r.status == 200
+		with open('extracted/talent_fixups.json', 'wb') as f:
+			f.write(r.read())
+	with open('extracted/talent_fixups.json', 'r', encoding='utf-8') as f:
+		talent_fixups = json.load(f)
+
+	with open('extracted/ExportedProject/Assets/Resources/local/en_us/TALENT.txt',
+			'r', encoding='utf-8') as f:
+		talent_strings: dict[str, str] = {}
+		for line in f:
+			if line == '\n' or line == 'END':
+				continue
+			key, value = line.rstrip('\n').split('=', 1)
+			assert key not in talent_strings
+			talent_strings[key] = value
+	with open('extracted/ExportedProject/Assets/Resources/gamedata/talent/TalentData.json',
+			'r', encoding='utf-8') as f:
+		talent_data = json.load(f)['Talents']
+
+	try:
+		os.mkdir('static/talents')
+	except FileExistsError:
+		pass
+	for talent in talent_data:
+		try:
+			name = talent_strings[talent['TalentName']]
+		except KeyError:
+			continue
+		try:
+			fixup = talent_fixups[name]
+			icon_path = fixup['iconPath']
+		except KeyError:
+			icon_path = 'Assets/Resources/image/talents/%s.png' % talent['Key']
+		os.link('extracted/ExportedProject/' + icon_path, 'static/talents/%s.png' % talent['Key'])
 
 if __name__ == '__main__':
 	main()
