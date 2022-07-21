17e3679cd56fe2b6a6f2ed0832ce46b05898f801
raylu <90059+raylu@users.noreply.github.com>
2022-07-20 22:26:45 -0700

    switch to AssetRipper

--- extract.py
+++ extract.py
@@ -1,53 +1,18 @@
 #!/usr/bin/env python3
 
 import os
-import struct
+import subprocess
+import sys
 
 def main():
+	asset_ripper_path = sys.argv[1]
+
 	try:
 		os.mkdir('extracted')
 	except FileExistsError:
 		pass
 
-	files = [
-		('ArtifactData', 0x200),
-		('SkillData', 0x200),
-		('TalentData', 0x200),
-		('AttributesData', 0x200),
-		('Bosses', 0x200),
-		('skill_info', 0x200),
-		('AT', 0x06e460b8),
-		('ATTRIBUTE', 0x06d8a090),
-		('ARTIFACT', 0x06e493e0),
-		('CONTEXT', 0x06e882b0),
-		('TALENT', 0x06c0d6f0),
-	]
-	with open('raw/resources.assets', 'rb') as asset_f:
-		for filename, offset in files:
-			print('extracting', filename)
-			d = read_file(asset_f, filename.encode('ascii'), offset)
-			with open('extracted/' + filename, 'w') as extracted_f:
-				extracted_f.write(d.decode('utf-8'))
-
-def read_file(f, filename, offset):
-	f.seek(offset, os.SEEK_SET)
-	while True:
-		buf = f.read(512 * 1024)
-		buf_offset = buf.find(b'\x00' * 2 + filename)
-		if buf_offset == -1 or buf_offset % 4 != 2:
-			f.seek(-32, os.SEEK_CUR)
-			offset += 512 * 1024 - 32
-		else:
-			offset += 2 + buf_offset
-			break
-
-	pad_bytes = (len(filename) + 3) // 4 * 4 - len(filename)
-	null_padding = b'\x00' * pad_bytes
-	f.seek(offset, os.SEEK_SET)
-	assert f.read(len(filename)+ pad_bytes) == filename + null_padding
-	(asset_len,) = struct.unpack('I', f.read(4))
-	return f.read(asset_len)
-
+	subprocess.run([asset_ripper_path, 'raw/', '-o' 'extracted'], check=True)
 
 if __name__ == '__main__':
 	main()
--- mini_healer_tools.py
+++ mini_healer_tools.py
@@ -86,14 +86,16 @@ def main():
 	global artifacts, strings, artifact_names, talents
 	strings = {}
 	for filename in ['ARTIFACT', 'ATTRIBUTE', 'CONTEXT', 'TALENT']:
-		with open('extracted/' + filename, 'r', encoding='utf-8') as f:
+		path = 'extracted/ExportedProject/Assets/Resources/local/en_us/%s.txt' % filename
+		with open(path, 'r', encoding='utf-8') as f:
 			for line in f:
 				if line == '\n' or line == 'END':
 					continue
 				key, value = line.rstrip('\n').split('=', 1)
 				assert key not in strings
 				strings[key] = value
-	with open('extracted/ArtifactData', 'r', encoding='utf-8') as f:
+	with open('extracted/ExportedProject/Assets/Resources/gamedata/artifact/ArtifactData.json',
+			'r', encoding='utf-8') as f:
 		artifact_data = json.load(f)['Artifacts']
 		artifacts = {}
 		artifact_names = collections.defaultdict(list)
@@ -104,7 +106,8 @@ def main():
 				continue
 			artifacts[artifact['Key']] = artifact
 			artifact_names[name].append(artifact['Key'])
-	with open('extracted/TalentData', 'r', encoding='utf-8') as f:
+	with open('extracted/ExportedProject/Assets/Resources/gamedata/talent/TalentData.json',
+			'r', encoding='utf-8') as f:
 		talent_data = json.load(f)['Talents']
 		talent_dict = {}
 		talent_strings = {}
